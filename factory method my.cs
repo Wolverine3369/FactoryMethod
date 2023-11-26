bb64905b83fb3bd04616ad90d8cab695502d7d8c
Wolverine3369 <Wolverine3369@users.noreply.github.com>
2023-11-26 19:38:55 +0100

    FactoryMethod1

--- factory method my.cs
+++ factory method my.cs
@@ -4,22 +4,33 @@ namespace Wolverine
 {
     interface ICar
     {
-        void Car();
+        void Model2022();
+        void Model2023();
     }
 
     class Ford : ICar
     {
-        public void Car()
+        public void Model2022()
         {
-            Console.WriteLine("I am the Ford and I was produced by the Ford Factory!");
+            Console.WriteLine("I am a Ford model 2022 and I was produced by the Ford Factory!");
+        }
+
+        public void Model2023()
+        {
+            Console.WriteLine("I am a Ford model 2023 and I was produced by the Ford Factory!");
         }
     }
 
     class Dodge : ICar
     {
-        public void Car()
+        public void Model2022()
         {
-            Console.WriteLine("I am the Dodge and I was produced by the Dodge Factory!");
+            Console.WriteLine("I am a Dodge model 2022 and I was produced by the Dodge Factory!");
+        }
+
+        public void Model2023()
+        {
+            Console.WriteLine("I am a Dodge model 2023 and I was produced by the Dodge Factory!");
         }
     }
 
@@ -46,11 +57,20 @@ namespace Wolverine
 
     class Program
     {
+        public static void Customer(IFactory factory)
+        {
+            ICar Car = factory.ProduceCar();
+            Car.Model2022();
+            Car.Model2023();
+        }
+
         static void Main()
         {
             IFactory DodgeFactory = new DodgeFactory();
-            ICar Dodge = DodgeFactory.ProduceCar();
-            Dodge.Car();
+            Customer(DodgeFactory);
+
+            IFactory FordFactory = new FordFactory();
+            Customer(FordFactory);
         }
     }
 }
